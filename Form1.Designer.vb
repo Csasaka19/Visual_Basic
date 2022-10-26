3bcd3f2a01a110ada0a7a04e78bb756a07b305af
Clive Sasaka <clivesasaka@gmail.com>
2022-10-26 19:04:14 +0300

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,283 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupStudentDetails = New System.Windows.Forms.GroupBox()
+        Me.txtoutput2 = New System.Windows.Forms.Label()
+        Me.txtoutput1 = New System.Windows.Forms.Label()
+        Me.cbo1 = New System.Windows.Forms.ComboBox()
+        Me.cmdSubmit = New System.Windows.Forms.Button()
+        Me.ListBoxHobbies = New System.Windows.Forms.ListBox()
+        Me.txtFullNames = New System.Windows.Forms.TextBox()
+        Me.LabelFullnames = New System.Windows.Forms.Label()
+        Me.lblYear = New System.Windows.Forms.Label()
+        Me.RadiobtnFemale = New System.Windows.Forms.RadioButton()
+        Me.RadiobtnMale = New System.Windows.Forms.RadioButton()
+        Me.txtMobileNumber = New System.Windows.Forms.TextBox()
+        Me.LabelHobbies = New System.Windows.Forms.Label()
+        Me.LabelMobileNumber = New System.Windows.Forms.Label()
+        Me.LabelEmailaddress = New System.Windows.Forms.Label()
+        Me.LabelGender = New System.Windows.Forms.Label()
+        Me.txtemailaddress = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.labelRegNo = New System.Windows.Forms.Label()
+        Me.txtRegistrationNumber = New System.Windows.Forms.TextBox()
+        Me.GroupStudentDetails.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupStudentDetails
+        '
+        Me.GroupStudentDetails.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.GroupStudentDetails.Controls.Add(Me.txtoutput2)
+        Me.GroupStudentDetails.Controls.Add(Me.txtoutput1)
+        Me.GroupStudentDetails.Controls.Add(Me.cbo1)
+        Me.GroupStudentDetails.Controls.Add(Me.cmdSubmit)
+        Me.GroupStudentDetails.Controls.Add(Me.ListBoxHobbies)
+        Me.GroupStudentDetails.Controls.Add(Me.txtFullNames)
+        Me.GroupStudentDetails.Controls.Add(Me.LabelFullnames)
+        Me.GroupStudentDetails.Controls.Add(Me.lblYear)
+        Me.GroupStudentDetails.Controls.Add(Me.RadiobtnFemale)
+        Me.GroupStudentDetails.Controls.Add(Me.RadiobtnMale)
+        Me.GroupStudentDetails.Controls.Add(Me.txtMobileNumber)
+        Me.GroupStudentDetails.Controls.Add(Me.LabelHobbies)
+        Me.GroupStudentDetails.Controls.Add(Me.LabelMobileNumber)
+        Me.GroupStudentDetails.Controls.Add(Me.LabelEmailaddress)
+        Me.GroupStudentDetails.Controls.Add(Me.LabelGender)
+        Me.GroupStudentDetails.Controls.Add(Me.txtemailaddress)
+        Me.GroupStudentDetails.Controls.Add(Me.Label4)
+        Me.GroupStudentDetails.Controls.Add(Me.labelRegNo)
+        Me.GroupStudentDetails.Controls.Add(Me.txtRegistrationNumber)
+        Me.GroupStudentDetails.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupStudentDetails.Location = New System.Drawing.Point(26, 12)
+        Me.GroupStudentDetails.Name = "GroupStudentDetails"
+        Me.GroupStudentDetails.Size = New System.Drawing.Size(1306, 698)
+        Me.GroupStudentDetails.TabIndex = 0
+        Me.GroupStudentDetails.TabStop = False
+        Me.GroupStudentDetails.Text = "Student Details"
+        '
+        'txtoutput2
+        '
+        Me.txtoutput2.AutoSize = True
+        Me.txtoutput2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtoutput2.Location = New System.Drawing.Point(997, 119)
+        Me.txtoutput2.Name = "txtoutput2"
+        Me.txtoutput2.Size = New System.Drawing.Size(207, 34)
+        Me.txtoutput2.TabIndex = 23
+        Me.txtoutput2.Text = "Second Name"
+        '
+        'txtoutput1
+        '
+        Me.txtoutput1.AutoSize = True
+        Me.txtoutput1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtoutput1.Location = New System.Drawing.Point(779, 119)
+        Me.txtoutput1.Name = "txtoutput1"
+        Me.txtoutput1.Size = New System.Drawing.Size(164, 34)
+        Me.txtoutput1.TabIndex = 22
+        Me.txtoutput1.Text = "First Name"
+        '
+        'cbo1
+        '
+        Me.cbo1.FormattingEnabled = True
+        Me.cbo1.Items.AddRange(New Object() {"2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"})
+        Me.cbo1.Location = New System.Drawing.Point(364, 573)
+        Me.cbo1.Name = "cbo1"
+        Me.cbo1.Size = New System.Drawing.Size(199, 40)
+        Me.cbo1.TabIndex = 7
+        '
+        'cmdSubmit
+        '
+        Me.cmdSubmit.Location = New System.Drawing.Point(730, 587)
+        Me.cmdSubmit.Name = "cmdSubmit"
+        Me.cmdSubmit.Size = New System.Drawing.Size(158, 77)
+        Me.cmdSubmit.TabIndex = 8
+        Me.cmdSubmit.Text = "Submit"
+        Me.cmdSubmit.UseVisualStyleBackColor = True
+        '
+        'ListBoxHobbies
+        '
+        Me.ListBoxHobbies.FormattingEnabled = True
+        Me.ListBoxHobbies.ItemHeight = 32
+        Me.ListBoxHobbies.Items.AddRange(New Object() {"ListBoxHobbies"})
+        Me.ListBoxHobbies.Location = New System.Drawing.Point(364, 203)
+        Me.ListBoxHobbies.Name = "ListBoxHobbies"
+        Me.ListBoxHobbies.Size = New System.Drawing.Size(227, 68)
+        Me.ListBoxHobbies.TabIndex = 2
+        '
+        'txtFullNames
+        '
+        Me.txtFullNames.Location = New System.Drawing.Point(313, 116)
+        Me.txtFullNames.Name = "txtFullNames"
+        Me.txtFullNames.Size = New System.Drawing.Size(379, 39)
+        Me.txtFullNames.TabIndex = 1
+        '
+        'LabelFullnames
+        '
+        Me.LabelFullnames.AutoSize = True
+        Me.LabelFullnames.Location = New System.Drawing.Point(62, 119)
+        Me.LabelFullnames.Name = "LabelFullnames"
+        Me.LabelFullnames.Size = New System.Drawing.Size(164, 32)
+        Me.LabelFullnames.TabIndex = 16
+        Me.LabelFullnames.Text = "Full names"
+        '
+        'lblYear
+        '
+        Me.lblYear.AutoSize = True
+        Me.lblYear.Location = New System.Drawing.Point(51, 576)
+        Me.lblYear.Name = "lblYear"
+        Me.lblYear.Size = New System.Drawing.Size(194, 32)
+        Me.lblYear.TabIndex = 15
+        Me.lblYear.Text = "Year of study"
+        '
+        'RadiobtnFemale
+        '
+        Me.RadiobtnFemale.AutoSize = True
+        Me.RadiobtnFemale.Location = New System.Drawing.Point(510, 308)
+        Me.RadiobtnFemale.Name = "RadiobtnFemale"
+        Me.RadiobtnFemale.Size = New System.Drawing.Size(141, 36)
+        Me.RadiobtnFemale.TabIndex = 4
+        Me.RadiobtnFemale.TabStop = True
+        Me.RadiobtnFemale.Text = "Female"
+        Me.RadiobtnFemale.UseVisualStyleBackColor = True
+        '
+        'RadiobtnMale
+        '
+        Me.RadiobtnMale.AutoSize = True
+        Me.RadiobtnMale.Location = New System.Drawing.Point(350, 308)
+        Me.RadiobtnMale.Name = "RadiobtnMale"
+        Me.RadiobtnMale.Size = New System.Drawing.Size(106, 36)
+        Me.RadiobtnMale.TabIndex = 3
+        Me.RadiobtnMale.TabStop = True
+        Me.RadiobtnMale.Text = "Male"
+        Me.RadiobtnMale.UseVisualStyleBackColor = True
+        '
+        'txtMobileNumber
+        '
+        Me.txtMobileNumber.Location = New System.Drawing.Point(364, 396)
+        Me.txtMobileNumber.Name = "txtMobileNumber"
+        Me.txtMobileNumber.Size = New System.Drawing.Size(199, 39)
+        Me.txtMobileNumber.TabIndex = 5
+        '
+        'LabelHobbies
+        '
+        Me.LabelHobbies.AutoSize = True
+        Me.LabelHobbies.Location = New System.Drawing.Point(78, 203)
+        Me.LabelHobbies.Name = "LabelHobbies"
+        Me.LabelHobbies.Size = New System.Drawing.Size(127, 32)
+        Me.LabelHobbies.TabIndex = 11
+        Me.LabelHobbies.Text = "Hobbies"
+        '
+        'LabelMobileNumber
+        '
+        Me.LabelMobileNumber.AutoSize = True
+        Me.LabelMobileNumber.Location = New System.Drawing.Point(51, 403)
+        Me.LabelMobileNumber.Name = "LabelMobileNumber"
+        Me.LabelMobileNumber.Size = New System.Drawing.Size(216, 32)
+        Me.LabelMobileNumber.TabIndex = 10
+        Me.LabelMobileNumber.Text = "Mobile number"
+        '
+        'LabelEmailaddress
+        '
+        Me.LabelEmailaddress.AutoSize = True
+        Me.LabelEmailaddress.Location = New System.Drawing.Point(51, 487)
+        Me.LabelEmailaddress.Name = "LabelEmailaddress"
+        Me.LabelEmailaddress.Size = New System.Drawing.Size(208, 32)
+        Me.LabelEmailaddress.TabIndex = 9
+        Me.LabelEmailaddress.Text = "Email address"
+        '
+        'LabelGender
+        '
+        Me.LabelGender.AutoSize = True
+        Me.LabelGender.Location = New System.Drawing.Point(78, 312)
+        Me.LabelGender.Name = "LabelGender"
+        Me.LabelGender.Size = New System.Drawing.Size(116, 32)
+        Me.LabelGender.TabIndex = 8
+        Me.LabelGender.Text = "Gender"
+        '
+        'txtemailaddress
+        '
+        Me.txtemailaddress.Location = New System.Drawing.Point(364, 480)
+        Me.txtemailaddress.Name = "txtemailaddress"
+        Me.txtemailaddress.Size = New System.Drawing.Size(199, 39)
+        Me.txtemailaddress.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(120, 253)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 32)
+        Me.Label4.TabIndex = 4
+        '
+        'labelRegNo
+        '
+        Me.labelRegNo.AutoSize = True
+        Me.labelRegNo.Location = New System.Drawing.Point(15, 51)
+        Me.labelRegNo.Name = "labelRegNo"
+        Me.labelRegNo.Size = New System.Drawing.Size(294, 32)
+        Me.labelRegNo.TabIndex = 1
+        Me.labelRegNo.Text = "Registration Number"
+        '
+        'txtRegistrationNumber
+        '
+        Me.txtRegistrationNumber.AllowDrop = True
+        Me.txtRegistrationNumber.Location = New System.Drawing.Point(364, 44)
+        Me.txtRegistrationNumber.Name = "txtRegistrationNumber"
+        Me.txtRegistrationNumber.Size = New System.Drawing.Size(199, 39)
+        Me.txtRegistrationNumber.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(1344, 706)
+        Me.Controls.Add(Me.GroupStudentDetails)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupStudentDetails.ResumeLayout(False)
+        Me.GroupStudentDetails.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupStudentDetails As GroupBox
+    Friend WithEvents cmdSubmit As Button
+    Friend WithEvents txtFullNames As TextBox
+    Friend WithEvents LabelFullnames As Label
+    Friend WithEvents lblYear As Label
+    Friend WithEvents RadiobtnFemale As RadioButton
+    Friend WithEvents RadiobtnMale As RadioButton
+    Friend WithEvents txtMobileNumber As TextBox
+    Friend WithEvents LabelHobbies As Label
+    Friend WithEvents LabelMobileNumber As Label
+    Friend WithEvents LabelEmailaddress As Label
+    Friend WithEvents LabelGender As Label
+    Friend WithEvents txtemailaddress As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents labelRegNo As Label
+    Friend WithEvents txtRegistrationNumber As TextBox
+    Friend WithEvents cbo1 As ComboBox
+    Friend WithEvents ListBoxHobbies As ListBox
+    Friend WithEvents txtoutput2 As Label
+    Friend WithEvents txtoutput1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,137 @@
+﻿
+Imports System.ComponentModel
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+
+    Private Sub cbo1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbo1.SelectedIndexChanged
+        With cbo1.Items
+            .Add("2016")
+            .Add("2017")
+            .Add("2018")
+            .Add("2019")
+            .Add("2020")
+            .Add("2021")
+            .Add("2022")
+            .Add("2023")
+        End With
+    End Sub
+
+    Private Sub ListBoxHobbies_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBoxHobbies.SelectedIndexChanged
+        ListBoxHobbies.Sorted = True
+        With ListBoxHobbies.Items
+            .Clear()
+            .Add("Swimming")
+            .Add("Hockey")
+            .Add("Chess")
+            .Add("Football")
+            .Add("Basketball")
+            .Add("Netball")
+            .Add("Student Leadership")
+            .Add("Volleyball")
+            .Add("Scrabble")
+            .Add("Gaming")
+            .Add("Handball")
+
+        End With
+    End Sub
+    Private Sub txtMobileNumber_TextChanged(sender As Object, e As EventArgs) Handles txtMobileNumber.TextChanged
+        Dim mobile As Integer
+        mobile = Integer.Parse(txtMobileNumber.Text)
+        txtMobileNumber.MaxLength = 10
+
+    End Sub
+
+    Private Sub txtRegistrationNumber_Validating(sender As Object, e As CancelEventArgs) Handles txtRegistrationNumber.Validating
+        If String.IsNullOrEmpty(txtRegistrationNumber.Text.Trim) Then
+            MessageBox.Show("Warning! Please input your registration number")
+        End If
+    End Sub
+    Private Sub txtRegistrationNumber_TextChanged(sender As Object, e As EventArgs) Handles txtRegistrationNumber.TextChanged
+        txtRegistrationNumber.MaxLength = 7
+
+    End Sub
+    Private Sub txtFullNames_TextChanged(sender As Object, e As EventArgs) Handles txtFullNames.TextChanged
+        txtFullNames.CharacterCasing = CharacterCasing.Upper
+    End Sub
+
+    Private Sub txtemailaddress_TextChanged(sender As Object, e As EventArgs) Handles txtemailaddress.TextChanged
+
+    End Sub
+
+    Private Sub cbo1_LostFocus(sender As Object, e As EventArgs) Handles cbo1.LostFocus
+        cbo1.BackColor = BackColor.Blue
+    End Sub
+
+    Private Sub cbo1_GotFocus(sender As Object, e As EventArgs) Handles cbo1.GotFocus
+        cbo1.BackColor = BackColor.White
+    End Sub
+
+    Private Sub ListBoxHobbies_LostFocus(sender As Object, e As EventArgs) Handles ListBoxHobbies.LostFocus
+        ListBoxHobbies.BackColor = BackColor.White
+    End Sub
+
+    Private Sub ListBoxHobbies_GotFocus(sender As Object, e As EventArgs) Handles ListBoxHobbies.GotFocus
+        ListBoxHobbies.BackColor = BackColor.Blue
+    End Sub
+
+    Private Sub txtMobileNumber_LostFocus(sender As Object, e As EventArgs) Handles txtMobileNumber.LostFocus
+        txtMobileNumber.BackColor = BackColor.White
+    End Sub
+
+    Private Sub txtMobileNumber_GotFocus(sender As Object, e As EventArgs) Handles txtMobileNumber.GotFocus
+        txtMobileNumber.BackColor = BackColor.Blue
+    End Sub
+
+    Private Sub txtRegistrationNumber_LostFocus(sender As Object, e As EventArgs) Handles txtRegistrationNumber.LostFocus
+        txtRegistrationNumber.BackColor = BackColor.White
+    End Sub
+
+    Private Sub txtRegistrationNumber_GotFocus(sender As Object, e As EventArgs) Handles txtRegistrationNumber.GotFocus
+        txtRegistrationNumber.BackColor = BackColor.Blue
+    End Sub
+
+    Private Sub txtemailaddress_LostFocus(sender As Object, e As EventArgs) Handles txtemailaddress.LostFocus
+        txtemailaddress.BackColor = BackColor.White
+    End Sub
+
+    Private Sub txtemailaddress_GotFocus(sender As Object, e As EventArgs) Handles txtemailaddress.GotFocus
+        txtemailaddress.BackColor = BackColor.Blue
+    End Sub
+
+    Private Sub txtFullNames_LostFocus(sender As Object, e As EventArgs) Handles txtFullNames.LostFocus
+        txtFullNames.BackColor = BackColor.White
+    End Sub
+
+    Private Sub txtFullNames_GotFocus(sender As Object, e As EventArgs) Handles txtFullNames.GotFocus
+        txtFullNames.BackColor = BackColor.Blue
+    End Sub
+
+    Private Sub RadiobtnMale_CheckedChanged(sender As Object, e As EventArgs) Handles RadiobtnMale.CheckedChanged
+
+    End Sub
+
+    Private Sub cmdSubmit_Click(sender As Object, e As EventArgs) Handles cmdSubmit.Click
+        Dim regex As Regex = New Regex("^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")
+        Dim isValid As Boolean = regex.IsMatch(txtemailaddress.Text.Trim)
+        If Not isValid Then
+            MessageBox.Show("Invalid Email.Please try again")
+        End If
+
+        Dim Fullname As String
+        Fullname = txtFullNames.Text.Trim
+
+        If Fullname.Contains(" ") Then
+            Dim parts As String() = Fullname.Split(" ")
+
+            txtoutput1.Text = parts(0)
+            txtoutput2.Text = parts(1)
+        Else
+            txtoutput1.Text = Fullname
+            txtoutput2.Text = ""
+        End If
+
+
+    End Sub
+End Class
